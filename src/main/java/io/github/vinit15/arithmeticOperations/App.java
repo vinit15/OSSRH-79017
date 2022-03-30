cddd21890d7dd179d3e29fca6109290320ac5dd9
vinit15 <vinitamolkale@gmail.com>
2022-03-30 09:28:56 +0530

    First commit

---
+++ src/main/java/io/github/vinit15/arithmeticOperations/App.java
@@ -0,0 +1,17 @@
+package io.github.vinit15.arithmeticOperations;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Demo from arithemtic operation class" );
+        System.out.println("Add of 10 and 20: "+ Operations.add(10,20));
+    }
+    
+    
+    
+}
---
+++ src/main/java/io/github/vinit15/arithmeticOperations/Operations.java
@@ -0,0 +1,21 @@
+package io.github.vinit15.arithmeticOperations;
+
+public class Operations {
+
+	public static int add(int a, int b) {
+    	return a+b;
+    }
+    
+    public static int sub(int a, int b) {
+    	return a-b;
+    }
+    
+    public static int mul(int a, int b) {
+    	return a*b;
+    }
+    
+    public static int div(int a, int b) {
+    	return a/b;
+    }
+	
+}
